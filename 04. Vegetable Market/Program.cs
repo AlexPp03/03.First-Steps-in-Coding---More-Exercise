d7d4a4400d57c8d2280f7c2c53420f4842d1671e
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 14:01:38 +0200

    Add files via upload

---
+++ 04. Vegetable Market/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+double priceV = double.Parse(Console.ReadLine());
+double priceF = double.Parse(Console.ReadLine());
+double kgV = double.Parse(Console.ReadLine());
+double kgF = double.Parse(Console.ReadLine());
+
+double fullPriceV = priceV * kgV;
+double fullPriceF = priceF * kgF;
+
+double finalPrice = fullPriceF + fullPriceV;
+double finalPriceEuro = finalPrice / 1.94;
+
+Console.WriteLine($"{finalPriceEuro:f2}");
